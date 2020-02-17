290754d055c75f3fbd78aa64c3e085200e3e565f
vipul9171 <vipul.9171@gmail.com>
2020-02-17 13:46:51 +0530

    Update SpringBootDemoApplication.java

--- SpringBootDemo/src/main/java/com/ibm/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/ibm/SpringBootDemo/SpringBootDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
